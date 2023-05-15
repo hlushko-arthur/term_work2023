90a4512a8fbc3f595ff1404178b205652f9bd604
Arthur Hlushko <61018089+hlushko-arthur@users.noreply.github.com>
2023-05-16 00:58:45 +0300

    Update schema.js

--- server/student/schema.js
+++ server/student/schema.js
@@ -10,15 +10,4 @@ var schema = mongoose.Schema({
 	additionalInfo: String,
 });
 
-schema.methods.create = function (obj, user, sd) {
-	this.fullName = obj.fullName,
-		this.educationalProgram = obj.educationalProgram,
-		this.completionYear = obj.completionYear,
-		this.studyForm = obj.studyForm,
-		this.studyType = obj.studyType,
-		this.groupCode = obj.fullName,
-		this.studentsNumber = obj.studentsNumber,
-		this.additionalInfo = obj.additionalInfo
-}
-
 module.exports = mongoose.model('Student', schema);
